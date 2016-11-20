bbbe8c3add34e978b2a4bfaf9746f065b1e5aae8
phamm <phamm@PWILLI>
2016-11-20 12:54:49 +0100

    Project init

---
+++ src/main/java/calculator/Main.java
@@ -0,0 +1,15 @@
+package calculator;
+
+public class Main
+{
+	/**
+	 * 
+	 * @param i - first int
+	 * @param j - second int
+	 * @return - sum
+	 */
+	public int berechne(int i, int j)
+	{
+		return i+j;
+	}
+}
