20b2640c66b02bfaa0e4d2ab0bde390cb79d8d95
phamm <pwilli@tsn.at>
2016-12-07 10:46:32 +0100

    Added some Tests

--- src/test/java/calculator/JunitTest.java
+++ src/test/java/calculator/JunitTest.java
@@ -6,15 +6,10 @@ import junit.framework.*;
 public class JunitTest extends TestCase
 {
 	protected double n1,n2, expectedresult;
-	//Add a = new Add(2,2);
 	protected void setUp()
 	{
 		n1 = 6.0;
 		n2 = 2.0;
-		
-		
-		Multiply mul = new Multiply(n1, n2);
-		Divide div = new Divide(n1, n2);
 	}
 	
 	public void testAdd()
@@ -31,4 +26,24 @@ public class JunitTest extends TestCase
 		expectedresult = 4;
 		assertEquals(expectedresult, sub.calculate());
 	}
+	
+	public void testDivision()
+	{
+		Divide div = new Divide(n1, n2);
+		expectedresult = 3;
+		assertEquals(expectedresult, div.calculate());
+	}
+	
+	public void testMultiply()
+	{
+		Multiply mul = new Multiply(n1, n2);
+		expectedresult = 12;
+		assertEquals(expectedresult, mul.calculate());
+	}
+	
+	public void testParser()
+	{
+		assertEquals(4.0, Parser.toDouble("4"));
+	}
+	
 }
